91d5a4350ff441557bfec76035a5b06bee2cbc7b
silas <silas@silas.link>
2022-12-01 10:33:14 +0100

    updated dist directory

--- dist/JSONType.d.ts
+++
@@ -1,6 +0,0 @@
-import type { SyncModel } from "./SyncModel";
-import { ExcludePropertyType, OnlyPropertyType, ReplacePropertyType } from "js-helper";
-export declare type JSONSyncModel<Model extends SyncModel> = {
-    columns: ReplacePropertyType<ExcludePropertyType<ExcludePropertyType<Model, Function>, SyncModel>, Date, string>;
-    relations: OnlyPropertyType<Model, SyncModel> & OnlyPropertyType<Model, SyncModel[]>;
-};
--- dist/decorators/ServerUpdateDateColumn.d.ts
+++
@@ -1,3 +0,0 @@
-import type { SyncModel } from '../SyncModel';
-import { ColumnOptions } from 'typeorm';
-export declare function ServerUpdateDateColumn(options?: ColumnOptions): (object: SyncModel, propertyName: string) => void;
